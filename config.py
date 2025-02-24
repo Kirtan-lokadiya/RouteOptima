26b137a16b082e9f6962947795c834e4ae9485e9
harsheel55 <147084769+harsheel55@users.noreply.github.com>
2025-02-24 07:20:30 +0000

    change config file

--- config.py
+++ config.py
@@ -1,3 +1,15 @@
 class Config:
     SECRET_KEY = 'your-secret-key'
-    # Additional configuration variables can be added here.
+    MONGO_URI = 'mongodb+srv://harsheel:harsheel@auth.kfaj4.mongodb.net/test?retryWrites=true&w=majority&appName=auth'
+    
+    # Flask-Mail configuration
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'pawarharsh3378@gmail.com'
+    MAIL_PASSWORD = 'jqyy zljq okeh nwhy'
+    
+    # Twilio configuration for SMS
+    TWILIO_ACCOUNT_SID = 'AC9d38cc0ae337d55b13ab70e84d9891f6'
+    TWILIO_AUTH_TOKEN = '8924e50ac41dc0117255b7f286b0ec02'
+    TWILIO_PHONE_NUMBER = '+16824246736'
