de05c868cd21102cb3f047ffd3fc107dfa1dd205
Kirtan lokadiya <114921440+Kirtan-lokadiya@users.noreply.github.com>
2025-03-19 15:57:45 +0530

    readme file change

--- config.py
+++ config.py
@@ -13,7 +13,3 @@ class Config:
     TWILIO_ACCOUNT_SID = 'AC9d38cc0ae337d55b13ab70e84d9891f6'
     TWILIO_AUTH_TOKEN = '8924e50ac41dc0117255b7f286b0ec02'
     TWILIO_PHONE_NUMBER = '+16824246736'
-
-    GOOGLE_CLIENT_ID = '21115487922-a0kapdku67mea9d5jjmvanij65gqapdk.apps.googleusercontent.com'
-    GOOGLE_CLIENT_SECRET = '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'
-    GOOGLE_DISCOVERY_URL = 'https://accounts.google.com/.well-known/openid-configuration'
